bfed68de58ed330c5e0c72f21c187d2c8dfbfd20
Karthik Raju <karthikraju391@gmail.com>
2021-09-22 16:52:32 +0530

    Add new blogs

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const navToggle = document.getElementById('nav-toggle');
 const links = document.querySelector('.list-no-bullet-r');
 const reversed = document.querySelector('.reversed');
 
-let i = reversed.childNodes.length;
-while (i--) {
-   reversed.appendChild(reversed.childNodes[i]);
-}
+// let i = reversed.childNodes.length;
+// while (i--) {
+//    reversed.appendChild(reversed.childNodes[i]);
+// }
 
 navToggle.addEventListener('click', () => {
    links.classList.toggle('show-links');
